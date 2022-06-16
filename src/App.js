d3a34408f2afd5a4c8c81a5f90b2005b31101e16
AlexQ <78956966+Txintxor@users.noreply.github.com>
2022-06-16 10:19:31 +0200

    Limpiado de archvios y optimización de App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import Routes from "./application/routes.js";
 import NavBar from "./components/NavBar/NavBar.js";
-import "./App.css";
+import Home from "./components/Home/Home.js";
+import Naus from "./components/Naus/Naus.js";
+import Fitxa from "./components/Fitxa/Fitxa.js";
+import Login from "./components/Login/Login.js";
+import Pilots from "./components/Pilots/Pilots.js";
+import Pelis from "./components/Pelis/Pelis.js";
+import Register from "./components/Register/Register.js";
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Routes />
-    </div>
-  );
-}
+import { Button } from "./components/styled-c/styled-components.js";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => (
+  <div className="App">
+    <NavBar />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Naus" element={<Naus />} />
+        <Route path="/Fitxa" element={<Fitxa />} />
+        <Route path="/Pilots" element={<Pilots />} />
+        <Route path="/Pelis" element={<Pelis />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route
+          path="*"
+          element={<Button href="/">THE FORCE IS WEAK IN YOU 404!</Button>}
+        />
+      </Routes>
+    </Router>
+  </div>
+);
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
-import {Hom}  from "../styled-c/styled-components.js";
+import { Hom } from "../styled-c/styled-components.js";
 import image from "../../img/home.jpg";
 const Home = () => {
   return (
-    <Hom><a href="/Naus"><img src={image} alt="in a galaxy far far away " /></a></Hom>
-    );
-}
- 
+    <Hom>
+      <a href="/Naus">
+        <img src={image} alt="in a galaxy far far away " />
+      </a>
+    </Hom>
+  );
+};
+
 export default Home;
